62b0b02e6322432d80516692c5321b54136abac8
intrening <wol87@ya.ru>
2019-10-19 15:29:00 +0300

    delete unused get_related_posts_count

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 from django.db.models import Count
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
 
 def serialize_post(post):
     return {
