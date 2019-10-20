34db8afef0e3186565ae7345e9444f12157dd413
intrening <wol87@ya.ru>
2019-10-20 13:43:36 +0300

    not finish Prefetch posts count for tags

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 from django.db.models import Count
+from django.db.models import Prefetch
 
 def get_related_posts_count(tag):
     return tag.posts.count()
@@ -14,7 +15,7 @@ def serialize_post_optimized(post):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
+        "tags": [serialize_tag_uptimized(tag) for tag in post.tags.annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -38,22 +39,31 @@ def serialize_tag(tag):
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
+def serialize_tag_uptimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts__count,
+    }
 
 def index(request):
-
+    tags_prefetch = Prefetch('tags', Tag.objects.annotate(Count('posts')))
     most_popular_posts = Post.objects.popular() \
+                                     .prefetch_related(tags_prefetch) \
                                      .prefetch_related('author')[:5] \
                                      .fetch_with_comments_count()
 
-    fresh_posts = Post.objects.prefetch_related('author').annotate(Count('comments')).order_by('-published_at')
+    fresh_posts = Post.objects.prefetch_related('author') \
+                              .prefetch_related('tags') \
+                              .annotate(Count('comments')) \
+                              .order_by('-published_at')
     most_fresh_posts = list(fresh_posts)[:5]
 
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = Tag.objects.popular().annotate(Count('posts'))[:5]
 
     context = {
         'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
         'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag_uptimized(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
 
@@ -88,13 +98,14 @@ def post_detail(request, slug):
     most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = Post.objects.popular() \
+                                     .prefetch_related('tags') \
                                      .prefetch_related('author')[:5] \
                                      .fetch_with_comments_count()
 
     context = {
         'post': serialized_post,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
     }
     return render(request, 'post-details.html', context)
 
@@ -105,16 +116,17 @@ def tag_filter(request, tag_title):
     most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = Post.objects.popular() \
+                                     .prefetch_related('tags') \
                                      .prefetch_related('author')[:5] \
                                      .fetch_with_comments_count()
 
-    related_posts = tag.posts.all()[:20]
+    related_posts = tag.posts.all()[:20].fetch_with_comments_count()
 
     context = {
         "tag": tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        "posts": [serialize_post(post) for post in related_posts],
-        'most_popular_posts': [serialize_post(post) for post in most_popular_posts],
+        "posts": [serialize_post_optimized(post) for post in related_posts],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
     }
     return render(request, 'posts-list.html', context)
 
